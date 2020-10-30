c402efbff8b3b5e608bd1163b87ea0386c3f37f2
Mario Rivis <mario.rivis@gmail.com>
2020-10-30 19:01:21 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tmd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tag-my-dependency-fe';
+}
