de8dd2b8c6ac7d547ab0abd3db89c0e2aaf9c5b8
Mario Rivis <mario.rivis@gmail.com>
2020-10-31 01:57:07 +0200

    Setup commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from './providers/http.service';
+import {KeycloakService} from 'keycloak-angular';
 
 @Component({
   selector: 'tmd-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'tag-my-dependency-fe';
+
+  constructor(private httpService: HttpService,
+              private keycloakService: KeycloakService) {
+  }
+
+  ngOnInit(): void {
+    this.httpService.get('demo').subscribe(value => this.title = value.greeting);
+  }
+
+  logout(): void {
+    this.keycloakService.logout().then(value => console.log('logged out'));
+  }
+
+  profile(): void {
+    window.open(this.keycloakService.getKeycloakInstance().createAccountUrl(), '_blank');
+  }
+
+  login(): void {
+    this.keycloakService.login();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
+import {environment} from '../environments/environment';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {AuthGuard} from './providers/auth-guard.service';
+import {HttpClientModule} from '@angular/common/http';
+
+export function kcInitializer(keycloak: KeycloakService): () => Promise<any> {
+  return (): Promise<any> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await keycloak.init(environment.keycloakOptions);
+        console.log('Keycloak is initialized');
+        resolve();
+      } catch (error) {
+        console.log('Error thrown in init ' + error);
+        reject(error);
+      }
+    });
+  };
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +30,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
+    {provide: APP_INITIALIZER, useFactory: kcInitializer, multi: true, deps: [KeycloakService]},
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
